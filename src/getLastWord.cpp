d2f0df9d4a2b628c65822b35c694b3a26d7d7fff
sheshank3173 <sheshank3173@missionrnd.com>
2016-03-30 12:09:06 +0530

    Submit Commit

--- src/getLastWord.cpp
+++ src/getLastWord.cpp
@@ -9,7 +9,21 @@ Note:Dont modify original string Neglect Spaces at the right end and at left end
 ->Create a new string and return it , Use dynamic memory allocation .
 */
 #include <stdlib.h>
-#include <string.h>
+
 char * get_last_word(char * str){
-	return NULL;
+	int i, k, len = 0;
+	for (i = 0; str[i]; i++)
+		len++;
+	int j = len - 1;
+	char *str_new = (char *)malloc(len * sizeof(char));
+	for (i = len - 1; str[i] != ' '; i--)
+	{
+		j--;
+	}
+	for (i = j, k = 0; str[i] != ' '; k++, i--)
+	{
+		str_new[k] = str[i];
+	}
+	str_new[k] = '\0';
+	return str_new;
 }
